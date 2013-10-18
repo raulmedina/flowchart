b8bbc269a856bc2e157a77a96fe3018e108ac39d
Raul <blackbee@fullsail.edu>
2013-10-18 05:13:57 -0400

    changed folder name.

---
+++ Conditional_worlsheet/js/script.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+
+/*
+
+Student: Raul Medina
+October 2, 2013
+Version: 1.0A
+Assignment: Conditionals
+WPF-1
+
+
+*/
+
+
+//What is the terperature outside?
+//Is it hot or cold?
+var c = celcius  ;
+//Celcius.
+var f = farenheit ;
+//farenheit.
+
+if( temperature >25 ){
+     //It is cold outside
+     console.log("It is cold outside");
+}else{
+     //It is hot outside.
+if(temperature <25 )	
+     console.log("It is hot outside");
+} 
